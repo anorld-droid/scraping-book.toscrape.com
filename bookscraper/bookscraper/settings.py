e5970e7e02e5b5595b3635fc90a8dc99e4c7f7f3
anorld-droid <mulindipatrice00@gmail.com>
2023-07-17 09:31:33 +0300

    Use scrapeops.io for, Fake Headers & User Agents

--- bookscraper/bookscraper/settings.py
+++ bookscraper/bookscraper/settings.py
@@ -16,11 +16,16 @@ FEEDS = {
     'booksdata.json': {'format': 'json'}
 }
 
+SCRAPEOPS_API_KEY = '3af36694-bf71-4b13-a3a3-e1c6d01b1f24'
+SCRAPEOPS_FAKE_USER_AGENT_ENDPOINT = 'httpS://headers.scrapeops.io/v1/user-agents'
+SCRAPEOPS_FAKE_USER_AGENT_ENABLED = True
+SCRAPEOPS_NUM_RESULTS = 50
+
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 # USER_AGENT = "bookscraper (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS = 32
@@ -53,9 +58,11 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#    "bookscraper.middlewares.BookscraperDownloaderMiddleware": 543,
-# }
+DOWNLOADER_MIDDLEWARES = {
+    # "bookscraper.middlewares.BookscraperDownloaderMiddleware": 543,
+    "bookscraper.middlewares.ScrapeOpsFakeBrowserHeaderAgentMiddleware": 400,
+
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
@@ -67,7 +74,7 @@ ROBOTSTXT_OBEY = True
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     "bookscraper.pipelines.BookscraperPipeline": 300,
-    "bookscraper.pipelines.SaveToMySQLPipeline": 400,
+    # "bookscraper.pipelines.SaveToMySQLPipeline": 400,
 
 }
 
